ac2a41671b7f23037f3a57aaffe4814a47633953
careyesp95 <careyesp@libertadores.edu.co>
2023-04-27 10:47:37 -0500

    started lemon app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function Heading() {
+  return (
+    <h1>This is a H1 geading</h1>
+  );
+}
+
+
+function App() {
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
