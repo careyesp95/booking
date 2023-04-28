3b9a535347b7747ce2180fac50570582ae7cadf9
careyesp95 <careyesp@libertadores.edu.co>
2023-04-27 20:03:33 -0500

    settings Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
 import Navbar from '../components/Navbar.js';
+import Footer from '../components/Footer.js';
+import Slider from '../components/Slider'
+import {Link} from 'react-router-dom';
+import './Home.css';
 
 
 function Home() {
     return (
-        <Navbar />
-        
+        <>
+          <Navbar />
+          <main>
+            <div className="container-fluid main-section">
+                <div className="container-main ">
+                    <div className="row ">
+                        <div className="col text-start mx-5">
+                            <span className="span-text">Little Lemon</span>
+                            <p className="p-text">Chicago</p>
+                            <p style={{width:"20rem"}} className="text-white mx-5">We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                            <Link to="/booking">
+                                <button type="button" className="btn-text  mx-5 mb-4">Reserve a Table</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <Slider/>
+        </main>
+            <Footer />
+        </>
     )
 }
 
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,11 @@
 import Navbar from '../components/Navbar.js';
+import Footer from '../components/Footer.js';
 
 function Services(){
     return (
         <>
             <Navbar />
-
+            <Footer />
         </>
         
     )
