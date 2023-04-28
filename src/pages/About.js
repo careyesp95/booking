44d9e9a8c49432b8355e04f3ba03fe43d44c819f
careyesp95 <careyesp@libertadores.edu.co>
2023-04-28 18:14:38 -0500

    settings about and services

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ButtonHome from "../components/ButtonHome";
+import MainAbout from '../components/MainAbout';
 
 function About(){
     return (
         <>
             <Navbar />
             <ButtonHome />
+            <MainAbout />
             <Footer />
         </>
     )
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
 import ButtonHome from '../components/ButtonHome.js';
+import MainServices from '../components/MainServices.js';
 
 function Services(){
     return (
         <>
             <Navbar />
             <ButtonHome />
+            <MainServices />
             <Footer />
         </>
         
